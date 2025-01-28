31ca7d6788ad730a8d4da621bf445319d35582da
ngntu10 <phamnguyentu04@gmail.com>
2025-01-28 21:08:29 +0700

    feat: update design driven domain with infrastructure

--- GoLink-application/src/main/java/com/GoLink/services/event/Impl/EventAppServiceImpl.java
+++ GoLink-application/src/main/java/com/GoLink/services/event/Impl/EventAppServiceImpl.java
@@ -1,12 +1,19 @@
 package com.GoLink.services.event.Impl;
 
+import com.GoLink.services.HiDomainService;
 import com.GoLink.services.event.EventAppService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventAppServiceImpl implements EventAppService {
+    
+    // Call domain service
+    @Resource
+    private HiDomainService hiDomainService;
+    
     @Override
     public String sayHi(String name) {
-        return "Hello " + name;
+        return hiDomainService.sayHi(name);
     }
 }
--- GoLink-infrastructure/src/main/java/com/GoLink/persistence/repository/HiInfrasRepositoryImpl.java
+++ GoLink-infrastructure/src/main/java/com/GoLink/persistence/repository/HiInfrasRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.GoLink.persistence.repository;
 
 import com.GoLink.repository.HiDomainRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HiInfrasRepositoryImpl implements HiDomainRepository {
     @Override
     public String sayHi(String message) {
